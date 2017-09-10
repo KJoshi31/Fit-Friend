fb47ab8146eabdbae3bdce0d9511c602f4e555ad
Karan Joshi <karanjoshi31@live.com>
2017-09-09 20:46:38 -0500

    worked on exercise history UI

---
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/setup.java
@@ -0,0 +1,16 @@
+package edu.bu.fitnessfriend.fitnessfriend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karan on 9/9/2017.
+ */
+
+public class setup extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.setup);
+    }
+}
