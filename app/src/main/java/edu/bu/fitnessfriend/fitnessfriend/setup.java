efe8288b87ae721869260ab2c0bb0b143d731f6a
Karan Joshi <karanjoshi31@live.com>
2017-09-17 17:27:13 -0500

    created spinner data and populated spinners in UI. Also increased with of the add exercise linear layout container

--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/setup.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/setup.java
@@ -2,15 +2,32 @@ package edu.bu.fitnessfriend.fitnessfriend;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by karan on 9/9/2017.
  */
 
 public class setup extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_setup);
+
+        //created spinner for activity
+        Spinner activitySpinner = (Spinner) findViewById(R.id.activity_spinner);
+
+        //created array adapter using the string array and default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.activity_level_array, android.R.layout.simple_spinner_item);
+
+        //specify the layout to use when the choices appear on the screen
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //apply the adapter to the activity spinner
+        activitySpinner.setAdapter(adapter);
     }
 }
