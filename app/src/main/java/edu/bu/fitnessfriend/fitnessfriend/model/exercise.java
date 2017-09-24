c54993587e8d1e8128e42e7fdb6cbae20002325a
Karan Joshi <karanjoshi31@live.com>
2017-09-23 23:29:06 -0500

    added models for food, exercise, and setup

--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/model/exercise.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/model/exercise.java
@@ -5,4 +5,56 @@ package edu.bu.fitnessfriend.fitnessfriend.model;
  */
 
 public class exercise {
+
+    private String _exerciseName;
+    private double _calorie;
+    private double _duration;
+    private String _unit;
+
+    public exercise(){
+        _exerciseName = "";
+        _calorie = 0;
+        _duration = 0;
+        _unit = "";
+    }
+
+    public exercise(String exerciseName, double calorie, double duration, String unit) {
+        _exerciseName = exerciseName;
+        _calorie = calorie;
+        _duration = duration;
+        _unit = unit;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        _exerciseName = exerciseName;
+    }
+
+    public String getExerciseName(){
+        return _exerciseName;
+    }
+
+    public void setCalorie(double calorie){
+        _calorie = calorie;
+    }
+
+    public double getCalorie(){
+        return _calorie;
+    }
+
+    public void setDuration(double duration){
+        _duration = duration;
+    }
+
+    public double getDuration(){
+        return _duration;
+    }
+
+    public void setUnit(String unit){
+        _unit = unit;
+    }
+
+    public String getUnit(){
+        return _unit;
+    }
+
 }
--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/model/food.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/model/food.java
@@ -5,4 +5,45 @@ package edu.bu.fitnessfriend.fitnessfriend.model;
  */
 
 public class food {
+
+    private String _foodName;
+    private double _quantity;
+    private double _calorie;
+
+    public food(){
+        this._foodName = "";
+        this._quantity = 0;
+        this._calorie = 0;
+    }
+
+    public food(String foodName, double quantity, double calorie){
+        _foodName = foodName;
+        _quantity = quantity;
+        _calorie = calorie;
+    }
+
+    public void setFoodName(String foodName){
+        _foodName = foodName;
+    }
+
+    public String getFoodName(){
+        return _foodName;
+    }
+
+    public void setQuantity(double quantity){
+        _quantity = quantity;
+    }
+
+    public double getQuantity(){
+        return _quantity;
+    }
+
+    public void setCalorie(double calorie){
+        _calorie = calorie;
+    }
+
+    public double getCalorie() {
+        return _calorie;
+    }
+
 }
