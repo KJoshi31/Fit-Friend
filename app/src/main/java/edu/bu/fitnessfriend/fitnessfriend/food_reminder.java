c7e394a0be1387750acb0ebb75b82419e6caae12
Karan Joshi <karanjoshi31@live.com>
2017-10-03 23:19:55 -0500

    deleted old reminder related files and made new tweaks

---
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/food_reminder.java
@@ -0,0 +1,13 @@
+package edu.bu.fitnessfriend.fitnessfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class food_reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_reminder);
+    }
+}
