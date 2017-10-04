228cd644d8b07ffd36db789223e93141fe9af791
Karan Joshi <karanjoshi31@live.com>
2017-10-04 18:37:18 -0500

    added code to disable set reminder buttons on activity creation

--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/food_reminder.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/food_reminder.java
@@ -2,6 +2,7 @@ package edu.bu.fitnessfriend.fitnessfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class food_reminder extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class food_reminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_reminder);
+
+        Button setFoodReminderBtn = (Button) findViewById(R.id.set_food_reminder_btn);
+        setFoodReminderBtn.setEnabled(false);
     }
 }
