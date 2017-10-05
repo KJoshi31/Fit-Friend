eb4b79f6a4ff54b5c47fe0a91c5b23a918b11f76
Karan Joshi <karanjoshi31@live.com>
2017-10-04 19:20:26 -0500

    finished logic to get radio buttons

--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/exercise_reminder.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/exercise_reminder.java
@@ -2,9 +2,15 @@ package edu.bu.fitnessfriend.fitnessfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class exercise_reminder extends AppCompatActivity {
+
+    private boolean radioButtonSelected = false;
+    private String reminderType = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +20,33 @@ public class exercise_reminder extends AppCompatActivity {
         Button setExerciseReminderBtn = (Button) findViewById(R.id.set_ex_reminder_btn);
         setExerciseReminderBtn.setEnabled(false);
     }
+
+
+
+    public void selectReminder(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+        RadioButton texts = (RadioButton) findViewById(R.id.notif_ex_radio_btn);
+
+        switch (view.getId()){
+            case R.id.notif_ex_radio_btn:
+                if(checked){
+                    radioButtonSelected = true;
+                    reminderType = "notification";
+                }
+                break;
+
+            case R.id.txt_ex_radio_btn:
+                if(checked){
+                    radioButtonSelected = true;
+                    reminderType = "text message";
+                }
+                break;
+        }
+
+        Log.d("Radio button selected?",String.valueOf(radioButtonSelected));
+        Log.d("Reminder Type", reminderType);
+
+    }
+
+
 }
--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/food_reminder.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/food_reminder.java
@@ -2,9 +2,16 @@ package edu.bu.fitnessfriend.fitnessfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class food_reminder extends AppCompatActivity {
+
+    private boolean radioButtonSelected = false;
+    private String reminderType = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +21,31 @@ public class food_reminder extends AppCompatActivity {
         Button setFoodReminderBtn = (Button) findViewById(R.id.set_food_reminder_btn);
         setFoodReminderBtn.setEnabled(false);
     }
+
+
+    public void selectReminder(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()){
+            case R.id.notif_food_radio_btn:
+                if(checked){
+                    radioButtonSelected = true;
+                    reminderType = "notification";
+                }
+                break;
+
+            case R.id.txt_food_radio_btn:
+                if(checked){
+                    radioButtonSelected = true;
+                    reminderType = "text message";
+                }
+                break;
+        }
+
+        Log.d("Radio button selected?",String.valueOf(radioButtonSelected));
+        Log.d("Reminder Type", reminderType);
+
+    }
+
+
 }
