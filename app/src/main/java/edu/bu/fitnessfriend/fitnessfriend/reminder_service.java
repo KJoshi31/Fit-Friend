b71a374008c55c3d451d482b2e1b5c7808db5bb6
Karan Joshi <karanjoshi31@live.com>
2017-10-05 23:49:18 -0500

    created get permission function

--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/reminder_service.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/reminder_service.java
@@ -1,8 +1,10 @@
 package edu.bu.fitnessfriend.fitnessfriend;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.content.Context;
@@ -10,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 
 import org.joda.time.DateTime;
+
+import static android.support.v4.app.ActivityCompat.requestPermissions;
 
 /**
  * Created by karan on 10/4/2017.
@@ -34,6 +38,11 @@ public class reminder_service {
 
         return (sms == PackageManager.PERMISSION_GRANTED && phone == PackageManager.PERMISSION_GRANTED);
 
+    }
+
+    public static void getPhonePermissions(Activity activity){
+        String[] permissions = {Manifest.permission.SEND_SMS, Manifest.permission.READ_PHONE_STATE};
+        requestPermissions(activity,permissions,1);
     }
 
 }
