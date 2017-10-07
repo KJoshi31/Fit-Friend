8a7632246aff31e2cbef2fe774b75c3963b5350e
Karan Joshi <karanjoshi31@live.com>
2017-10-07 00:37:01 -0500

    added base sms notification to app, need to implement storing in db

--- app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/reminder_service.java
+++ app/src/main/java/edu/bu/fitnessfriend/fitnessfriend/reminder_service.java
@@ -2,7 +2,13 @@ package edu.bu.fitnessfriend.fitnessfriend;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
@@ -14,22 +20,94 @@ import android.view.View;
 import org.joda.time.DateTime;
 
 import static android.support.v4.app.ActivityCompat.requestPermissions;
+import static java.lang.Thread.sleep;
 
 /**
  * Created by karan on 10/4/2017.
  */
 
-public class reminder_service {
+public class reminder_service extends Service {
+
+    String reminderType = "";
+    String logType = "";
+    long waitMillis = 0L;
+
+    public reminder_service(){
+
+    }
+
+    @Override
+    public void onCreate(){
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+        final int currentId = startId;
+
+        logType = intent.getStringExtra("logType");
+        reminderType = intent.getStringExtra("reminderType");
+        waitMillis = Long.valueOf(intent.getLongExtra("millis",0L));
+
+        Log.d("Service reminderType",reminderType);
+        Log.d("waitMillis",String.valueOf(waitMillis));
+        waitMillis = Long.valueOf(intent.getLongExtra("millis",0L));
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                synchronized (this){
+                    try{
+                        SystemClock.sleep(waitMillis);
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    if(reminderType.equals("text message")){
+                        Log.d("hitting message","hitting sms");
+                        sendSmsMessage(getApplicationContext(), logType);
+                    }else{
+
+                    }
+                }
+
+               stopSelf();
+            }
+        };
+
+        Thread thread = new Thread(r);
+        thread.start();
 
 
-    public static void sendSmsMessage(Context c){
+
+        return Service.START_STICKY;
+    }
+
+
+    public static void sendSmsMessage(Context c, String logType){
+        String message = "";
+        if(logType.equals("food")){
+            message = "Friendly reminder from Fitness Friend\n" +
+                    "Please log your foods";
+        }else if(logType.equals("exercise")){
+            message = "Friendly reminder from Fitness Friend\n" +
+                    "Please log your exercises";
+        }else{
+            message = "Friendly reminder from Fitness Friend\n" +
+                    "Please log your foods & exercises";
+        }
+
+        Log.d("hi","from sms");
+
         TelephonyManager manager = (TelephonyManager)c.getApplicationContext()
                 .getSystemService(Context.TELEPHONY_SERVICE);
 
         String phoneNumber = manager.getLine1Number();
 
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber,null,"hi",null,null);
+        sms.sendTextMessage(phoneNumber,null,message,null,null);
     }
 
     public static boolean hasPhonePermissions(Context c){
@@ -45,4 +123,17 @@ public class reminder_service {
         requestPermissions(activity,permissions,1);
     }
 
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        logType = intent.getStringExtra("logType");
+        reminderType = intent.getStringExtra("reminderType");
+        waitMillis = Long.valueOf(intent.getLongExtra("millis",0L));
+        return null;
+    }
+
+    @Override
+    public void onDestroy(){
+
+    }
 }
